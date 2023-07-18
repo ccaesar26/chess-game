3791b9803993fd9d638fa7cdbdcd06b8a55bf387
Cezar Constăndoiu <cezarconstandoiu@gmail.com>
2023-07-18 11:55:54 +0300

    Update Queen.cpp

--- ChessLib/Queen.cpp
+++ ChessLib/Queen.cpp
@@ -8,117 +8,124 @@ Queen::Queen(EColor color)
 
 PositionList Queen::GetPattern(Position currentPos, std::function<PiecePtr(Position)> GetPieceFromBoard) const
 {
-	//// upper left
-	//for (int i = currentPos.row - 1, j = currentPos.col - 1; i >= 0 && j >= 0; i--, j--)
-	//{
-	//	Position toCheckPosition(i, j);
-	//	if (GetPieceFromBoard(toCheckPosition))
-	//	{
-	//		if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-	//		{
-	//			possibleMoves.insert(toCheckPosition);
-	//		}
-	//		break;
-	//	}
-	//	possibleMoves.insert(toCheckPosition);
-	//}
-	//// upper right
-	//for (int i = currentPos.row - 1, j = currentPos.col + 1; i >= 0 && j < 8; i--, j++)
-	//{
-	//	Position toCheckPosition(i, j);
-	//	if (GetPieceFromBoard(toCheckPosition))
-	//	{
-	//		if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-	//		{
-	//			possibleMoves.insert(toCheckPosition);
-	//		}
-	//		break;
-	//	}
-	//	possibleMoves.insert(toCheckPosition);
-	//}
-	//// lower left
-	//for (int i = currentPos.row + 1, j = currentPos.col - 1; i < 8 && j >= 0; i++, j--)
-	//{
-	//	Position toCheckPosition(i, j);
-	//	if (GetPieceFromBoard(toCheckPosition))
-	//	{
-	//		if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-	//		{
-	//			possibleMoves.insert(toCheckPosition);
-	//		}
-	//		break;
-	//	}
-	//	possibleMoves.insert(toCheckPosition);
-	//}
-	//// lower right
-	//for (int i = currentPos.row + 1, j = currentPos.col + 1; i < 8 && j < 8; i++, j++)
-	//{
-	//	Position toCheckPosition(i, j);
-	//	if (GetPieceFromBoard(toCheckPosition))
-	//	{
-	//		if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-	//		{
-	//			possibleMoves.insert(toCheckPosition);
-	//		}
-	//		break;
-	//	}
-	//	possibleMoves.insert(toCheckPosition);
-	//}
-	//// up
-	//for (int iRow = currentPos.row - 1; iRow >= 0; iRow--)
-	//{
-	//	Position toCheckPosition(iRow, currentPos.col);
-	//	if (GetPieceFromBoard(toCheckPosition))
-	//	{
-	//		if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-	//		{
-	//			possibleMoves.insert(toCheckPosition);
-	//		}
-	//		break;
-	//	}
-	//	else possibleMoves.insert(toCheckPosition);
-	//}
-	//// down
-	//for (int iRow = currentPos.row + 1; iRow <= 7; iRow++)
-	//{
-	//	Position toCheckPosition(iRow, currentPos.col);
-	//	if (GetPieceFromBoard(toCheckPosition))
-	//	{
-	//		if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-	//		{
-	//			possibleMoves.insert(toCheckPosition);
-	//		}
-	//		break;
-	//	}
-	//	else possibleMoves.insert(toCheckPosition);
-	//}
-	//// left
-	//for (int iCol = currentPos.col - 1; iCol >= 0; iCol--)
-	//{
-	//	Position toCheckPosition(currentPos.row, iCol);
-	//	if (GetPieceFromBoard(toCheckPosition))
-	//	{
-	//		if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-	//		{
-	//			possibleMoves.insert(toCheckPosition);
-	//		}
-	//		break;
-	//	}
-	//	else possibleMoves.insert(toCheckPosition);
-	//}
-	//// right
-	//for (int iCol = currentPos.col + 1; iCol <= 7; iCol++)
-	//{
-	//	Position toCheckPosition(currentPos.row, iCol);
-	//	if (GetPieceFromBoard(toCheckPosition))
-	//	{
-	//		if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-	//		{
-	//			possibleMoves.insert(toCheckPosition);
-	//		}
-	//		break;
-	//	}
-	//	else possibleMoves.insert(toCheckPosition);
-	//}
-	return PositionList();
+	PositionList finalPositions;
+
+	// upper left
+	for (int i = currentPos.row - 1, j = currentPos.col - 1; i >= 0 && j >= 0; i--, j--)
+	{
+		Position toCheckPosition(i, j);
+		if (GetPieceFromBoard(toCheckPosition))
+		{
+			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
+			{
+				finalPositions.push_back(toCheckPosition);
+			}
+			break;
+		}
+		finalPositions.push_back(toCheckPosition);
+	}
+	// upper right
+	for (int i = currentPos.row - 1, j = currentPos.col + 1; i >= 0 && j < 8; i--, j++)
+	{
+		Position toCheckPosition(i, j);
+		if (GetPieceFromBoard(toCheckPosition))
+		{
+			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
+			{
+				finalPositions.push_back(toCheckPosition);
+			}
+			break;
+		}
+		finalPositions.push_back(toCheckPosition);
+	}
+	// lower left
+	for (int i = currentPos.row + 1, j = currentPos.col - 1; i < 8 && j >= 0; i++, j--)
+	{
+		Position toCheckPosition(i, j);
+		if (GetPieceFromBoard(toCheckPosition))
+		{
+			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
+			{
+				finalPositions.push_back(toCheckPosition);
+			}
+			break;
+		}
+		finalPositions.push_back(toCheckPosition);
+	}
+	// lower right
+	for (int i = currentPos.row + 1, j = currentPos.col + 1; i < 8 && j < 8; i++, j++)
+	{
+		Position toCheckPosition(i, j);
+		if (GetPieceFromBoard(toCheckPosition))
+		{
+			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
+			{
+				finalPositions.push_back(toCheckPosition);
+			}
+			break;
+		}
+		finalPositions.push_back(toCheckPosition);
+	}
+
+	// up
+	for (int i = currentPos.row - 1; i >= 0; i--)
+	{
+		Position toCheckPosition(i, currentPos.col);
+		if (GetPieceFromBoard(toCheckPosition))
+		{
+			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
+			{
+				finalPositions.push_back(toCheckPosition);
+			}
+			break;
+		}
+		finalPositions.push_back(toCheckPosition);
+	}
+
+	// down
+	for (int i = currentPos.row + 1; i < 8; i++)
+	{
+		Position toCheckPosition(i, currentPos.col);
+		if (GetPieceFromBoard(toCheckPosition))
+		{
+			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
+			{
+				finalPositions.push_back(toCheckPosition);
+			}
+			break;
+		}
+		finalPositions.push_back(toCheckPosition);
+	}
+
+	// left
+	for (int j = currentPos.col - 1; j >= 0; j--)
+	{
+		Position toCheckPosition(currentPos.row, j);
+		if (GetPieceFromBoard(toCheckPosition))
+		{
+			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
+			{
+				finalPositions.push_back(toCheckPosition);
+			}
+			break;
+		}
+		finalPositions.push_back(toCheckPosition);
+	}
+
+	// right
+	for (int j = currentPos.col + 1; j < 8; j++)
+	{
+		Position toCheckPosition(currentPos.row, j);
+		if (GetPieceFromBoard(toCheckPosition))
+		{
+			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
+			{
+				finalPositions.push_back(toCheckPosition);
+			}
+			break;
+		}
+		finalPositions.push_back(toCheckPosition);
+	}
+
+	return finalPositions;
 }
