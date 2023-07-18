75c66d48126bcc284db3dcffb4150434549cafa4
Cezar Constăndoiu <cezarconstandoiu@gmail.com>
2023-07-18 11:48:06 +0300

    Update Pawn.cpp

--- ChessLib/Pawn.cpp
+++ ChessLib/Pawn.cpp
@@ -7,55 +7,58 @@ Pawn::Pawn(EColor color)
 
 PositionList Pawn::GetPattern(Position currentPos, std::function<PiecePtr(Position)> GetPieceFromBoard) const
 {
-	return PositionList();
-}
+	PositionList finalPositions;
 
-//void Pawn::GetMovesPossible(Position currentPos, std::function<IPiecePtr(Position)> GetPieceFromBoard, PositionPieceSet& possibleMoves) const
-//{
-//	int movingWay = 1;
-//	int initialLine = 1;
-//
-//	if (m_color == EColor::White)
-//	{
-//		movingWay = -1;
-//		initialLine = 6;
-//	}
-//
-//	if (GetPieceFromBoard(Position(currentPos.row + movingWay, currentPos.col)) == nullptr)
-//	{
-//		possibleMoves.insert(Position(currentPos.row + movingWay, currentPos.col));
-//	}
-//
-//	if (currentPos.col - 1 >= 0)
-//	{
-//		if (GetPieceFromBoard(Position(currentPos.row + movingWay, currentPos.col - 1)) != nullptr)
-//		{
-//			if (GetPieceFromBoard(Position(currentPos.row + movingWay, currentPos.col - 1))->GetColor() != m_color)
-//			{
-//				possibleMoves.insert(Position(currentPos.row + movingWay, currentPos.col - 1));
-//			}
-//		}
-//	}
-//
-//	if (currentPos.col + 1 <= 7)
-//	{
-//		if (GetPieceFromBoard(Position(currentPos.row + movingWay, currentPos.col + 1)) != nullptr)
-//		{
-//			if (GetPieceFromBoard(Position(currentPos.row + movingWay, currentPos.col + 1))->GetColor() != m_color)
-//			{
-//				possibleMoves.insert(Position(currentPos.row + movingWay, currentPos.col + 1));
-//			}
-//		}
-//	}
-//
-//	if (currentPos.row == initialLine)
-//	{
-//		if (possibleMoves.find(Position(currentPos.row + movingWay, currentPos.col)) != possibleMoves.end())
-//		{
-//			if (GetPieceFromBoard(Position(currentPos.row + 2 * movingWay, currentPos.col)) == nullptr)
-//			{
-//				possibleMoves.insert(Position(currentPos.row + 2 * movingWay, currentPos.col));
-//			}
-//		}
-//	}
-//}
+	int movingWay = 1, initialLine = 1;
+	if (m_color == EColor::White)
+	{
+		movingWay = -1;
+		initialLine = 6;
+	}
+	
+	Position oneSquareForwardPos = Position(currentPos.row + movingWay, currentPos.col);
+	Position twoSquaresForwardPos = Position(currentPos.row + 2 * movingWay, currentPos.col);
+	Position leftDiagonal = Position(currentPos.row + movingWay, currentPos.col - 1);
+	Position rightDiagonal = Position(currentPos.row + movingWay, currentPos.col + 1);
+
+	// One square forward
+	if (!GetPieceFromBoard(oneSquareForwardPos))
+	{
+		finalPositions.push_back(oneSquareForwardPos);
+
+		// Two squares forward
+		if (currentPos.row == initialLine)
+		{
+			if (!GetPieceFromBoard(twoSquaresForwardPos))
+			{
+				finalPositions.push_back(twoSquaresForwardPos);
+			}
+		}
+	}
+
+	// Left capture
+	if (currentPos.col - 1 >= 0)
+	{
+		if (GetPieceFromBoard(leftDiagonal))
+		{
+			if (GetPieceFromBoard(leftDiagonal)->GetColor() != m_color)
+			{
+				finalPositions.push_back(leftDiagonal);
+			}
+		}
+	}
+
+	// Right capture
+	if (currentPos.col + 1 <= 7)
+	{
+		if (GetPieceFromBoard(rightDiagonal))
+		{
+			if (GetPieceFromBoard(rightDiagonal)->GetColor() != m_color)
+			{
+				finalPositions.push_back(rightDiagonal);
+			}
+		}
+	}
+
+	return finalPositions;
+}
