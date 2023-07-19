ba8867d622ce2ec15ac04a1583230b8e28b1e6bc
Cezar Constăndoiu <cezarconstandoiu@gmail.com>
2023-07-19 09:27:04 +0300

    Update KingInCheckState.cpp

--- ChessTests/KingInCheckState.cpp
+++ ChessTests/KingInCheckState.cpp
@@ -2,19 +2,14 @@
 
 #include "ChessGame.h"
 
-//std::array<std::array<char, 8>, 8> arr = {
-//	{' ',' ',' ',' ',' ',' ',' ',' '},
-//	{' ',' ',' ',' ',' ',' ',' ',' '},
-//	{' ',' ',' ',' ',' ',' ',' ',' '},
-//	{' ',' ',' ',' ',' ',' ',' ',' '},
-//	{' ',' ',' ',' ',' ',' ',' ',' '},
-//	{' ',' ',' ',' ',' ',' ',' ',' '},
-//	{' ',' ',' ',' ',' ',' ',' ',' '},
-//	{' ',' ',' ',' ',' ',' ',' ',' '}
-//}
-//ChessGame g;
+
 
 TEST(KingInCheckState, ByHorse)
 {
-
+	ChessGame g;
+	g.MakeMovement('f', 2, 'f', 3);
+	g.MakeMovement('e', 7, 'e', 6);
+	g.MakeMovement('a', 2, 'a', 3);
+	g.MakeMovement('d', 8, 'h', 4);
+	g.MakeMovement('e', 1, 'f', 2);
 }
