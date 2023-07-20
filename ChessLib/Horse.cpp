11cc1d96199ea0b662cd4bd0d7132bd68fc5b444
Cezar Constăndoiu <cezarconstandoiu@gmail.com>
2023-07-20 08:25:25 +0300

    GetFinalPositionsFromMovementArray method

--- ChessLib/Horse.cpp
+++ ChessLib/Horse.cpp
@@ -8,8 +8,6 @@ Horse::Horse(EColor color)
 
 PositionList Horse::GetPattern(Position currentPos, std::function<PiecePtr(Position)> GetPieceFromBoard) const
 {
-	PositionList finalPositions;
-
 	std::array<Position, 8> possibleMovements = {
 		Position(currentPos.row + 1, currentPos.col - 2),
 		Position(currentPos.row + 2, currentPos.col - 1),
@@ -21,23 +19,5 @@ PositionList Horse::GetPattern(Position currentPos, std::function<PiecePtr(Posit
 		Position(currentPos.row - 1, currentPos.col - 2)
 	};
 
-	for (auto toCheckPosition : possibleMovements)
-	{
-		if (toCheckPosition.row < 0 || toCheckPosition.row > 7 ||
-			toCheckPosition.col < 0 || toCheckPosition.col > 7)
-		{
-			continue;
-		}
-		if (GetPieceFromBoard(toCheckPosition))
-		{
-			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-			{
-				finalPositions.push_back(toCheckPosition);
-			}
-			continue;
-		}
-		finalPositions.push_back(toCheckPosition);
-	}
-
-	return finalPositions;
+	return GetFinalPositionsFromMovementArray(possibleMovements, GetPieceFromBoard);
 }
--- ChessLib/King.cpp
+++ ChessLib/King.cpp
@@ -7,8 +7,6 @@ King::King(EColor color)
 
 PositionList King::GetPattern(Position currentPos, std::function<PiecePtr(Position)> GetPieceFromBoard) const
 {
-	PositionList finalPositions;
-
 	std::array<Position, 8> possibleMovements = {
 		Position(currentPos.row + 1, currentPos.col - 1),
 		Position(currentPos.row + 1, currentPos.col),
@@ -20,23 +18,5 @@ PositionList King::GetPattern(Position currentPos, std::function<PiecePtr(Positi
 		Position(currentPos.row    , currentPos.col + 1)
 	};
 
-	for (auto toCheckPosition : possibleMovements)
-	{
-		if (toCheckPosition.row < 0 || toCheckPosition.row > 7 ||
-			toCheckPosition.col < 0 || toCheckPosition.col > 7)
-		{
-			continue;
-		}
-		if (GetPieceFromBoard(toCheckPosition))
-		{
-			if (GetPieceFromBoard(toCheckPosition)->GetColor() != m_color)
-			{
-				finalPositions.push_back(toCheckPosition);
-			}
-			continue;
-		}
-		finalPositions.push_back(toCheckPosition);
-	}
-
-	return finalPositions;
+	return GetFinalPositionsFromMovementArray(possibleMovements, GetPieceFromBoard);
 }
