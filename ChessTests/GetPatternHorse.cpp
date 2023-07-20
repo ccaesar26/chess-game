aa434f102cbdb1baa0a26601504d390c1864d0eb
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-20 11:33:19 +0300

    Fix logic error in every test for horse
    Create test 4 for horse

--- ChessTests/GetPatternHorse.cpp
+++ ChessTests/GetPatternHorse.cpp
@@ -27,7 +27,7 @@ TEST(HorsePossibleMoves, Test1)
 	PositionList horsepossibleMoves = game.GetPossibleMoves(Position(4,3));
 	PositionList expectedHorseMoves = { Position(2,2),Position(2, 4) ,Position(3, 1) ,Position(3, 5), Position(5, 1) ,Position(5, 5),Position(6, 2),Position(6, 4) };
 
-	for (int i = 0; i < horsepossibleMoves.size(); i++)
+	for (int i = 0; i < expectedHorseMoves.size(); i++)
 	{
 		EXPECT_EQ(horsepossibleMoves[i], expectedHorseMoves[i]);
 	}
@@ -84,7 +84,38 @@ TEST(HorsePossibleMoves, Test3)
 	PositionList horsepossibleMoves = game.GetPossibleMoves(Position(4, 3));
 	PositionList expectedHorseMoves = { Position(2,2),Position(2, 4) ,Position(3, 1) ,Position(3, 5), Position(5, 1) ,Position(5, 5),Position(6, 2),Position(6, 4) };
 
-	for (int i = 0; i < horsepossibleMoves.size(); i++)
+	for (int i = 0; i < expectedHorseMoves.size(); i++)
+	{
+		EXPECT_EQ(horsepossibleMoves[i], expectedHorseMoves[i]);
+	}
+}
+
+TEST(HorsePossibleMoves, Test4)
+{
+	// Declare board // 
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			'k', ' ', ' ', ' ', ' ', ' ', ' ', 'h'    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	PositionList horsepossibleMoves = game.GetPossibleMoves(Position(7, 7));
+	PositionList expectedHorseMoves = { Position(5,6),Position(6, 5) };
+
+	for (int i = 0; i < expectedHorseMoves.size(); i++)
 	{
 		EXPECT_EQ(horsepossibleMoves[i], expectedHorseMoves[i]);
 	}
