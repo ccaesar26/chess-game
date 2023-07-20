2bb99f4379d2b57bdfb15d255bc68de95fb63a3f
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-20 12:42:45 +0300

    Create Test 10 for GetPossibleMoves for horse

--- ChessTests/GetPatternHorse.cpp
+++ ChessTests/GetPatternHorse.cpp
@@ -275,3 +275,34 @@ TEST(HorsePossibleMoves, Test9)
 		EXPECT_EQ(horsepossibleMoves[i], expectedHorseMoves[i]);
 	}
 }
+
+TEST(HorsePossibleMoves, Test10)
+{
+	// Declare board // 
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', 'h', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', 'H', ' ', ' ', ' ', ' ', ' ',   // 6
+			'k', ' ', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	PositionList horsepossibleMoves = game.GetPossibleMoves(Position(4, 1));
+	PositionList expectedHorseMoves = { Position(6,2) };
+
+	for (int i = 0; i < expectedHorseMoves.size(); i++)
+	{
+		EXPECT_EQ(horsepossibleMoves[i], expectedHorseMoves[i]);
+	}
+}
