db9d282d065b38030fc394cb65e4deeeb832aa06
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-20 14:43:41 +0300

    Create Test 13 for horse

--- ChessTests/GetPatternHorse.cpp
+++ ChessTests/GetPatternHorse.cpp
@@ -376,3 +376,29 @@ TEST(HorsePossibleMoves, Test12)
 	PositionList horsepossibleMoves = game.GetPossibleMoves(Position(4, 3));
 	EXPECT_EQ(horsepossibleMoves.size(), 0);
 }
+
+TEST(HorsePossibleMoves, Test13)
+{
+	// Declare board // 
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', 'h', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', 'H', ' ', ' ', ' ', ' ', ' ',   // 6
+			'k', ' ', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	PositionList horsepossibleMoves = game.GetPossibleMoves(Position(5, 2));
+	EXPECT_EQ(horsepossibleMoves.size(), 0);
+}
