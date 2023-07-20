f304c3b738c3aad85a106b03e53d688746437791
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-20 11:13:49 +0300

    Create test 3 for GetPossibleMoves for horse

--- ChessTests/GetPatternHorse.cpp
+++ ChessTests/GetPatternHorse.cpp
@@ -4,17 +4,21 @@
 
 TEST(HorsePossibleMoves, Test1)
 {
-	std::array<std::array<char, 8>, 8> alternativeBoard;
+	// Declare board //
 
-	// Declare board // 
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
 
-	for (int i = 0; i < 8; i++)
-		for (int j = 0; j < 8; j++)
-			alternativeBoard[i][j] = ' ';
-
-	alternativeBoard[4][3] = 'h';
-	alternativeBoard[0][0] = 'k';
-	alternativeBoard[7][7] = 'K';
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', 'h', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			'k', ' ', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
 
 	// Tests //
 
@@ -53,4 +57,35 @@ TEST(HorsePossibleMoves, Test2)
 
 	PositionList horsepossibleMoves = game.GetPossibleMoves(Position(4, 3));
 	EXPECT_EQ(horsepossibleMoves.size(), 0);
+}
+
+TEST(HorsePossibleMoves, Test3)
+{
+	// Declare board // 
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', 'P', ' ', 'P', ' ', ' ', ' ',   // 2
+			' ', 'P', ' ', ' ', ' ', 'P', ' ', ' ',   // 3
+			' ', ' ', ' ', 'h', ' ', ' ', ' ', ' ',   // 4
+			' ', 'P', ' ', ' ', ' ', 'P', ' ', ' ',   // 5
+			' ', ' ', 'P', ' ', 'P', ' ', ' ', ' ',   // 6
+			'k', ' ', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	PositionList horsepossibleMoves = game.GetPossibleMoves(Position(4, 3));
+	PositionList expectedHorseMoves = { Position(2,2),Position(2, 4) ,Position(3, 1) ,Position(3, 5), Position(5, 1) ,Position(5, 5),Position(6, 2),Position(6, 4) };
+
+	for (int i = 0; i < horsepossibleMoves.size(); i++)
+	{
+		EXPECT_EQ(horsepossibleMoves[i], expectedHorseMoves[i]);
+	}
 }
