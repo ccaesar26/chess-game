557e9b8742c90c14037ace3f5b0a068420afa840
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-20 18:16:36 +0300

    Create Test_Moves_While_King_In_Check_From_Rook for bishop

--- ChessTests/TestBishopPossibleMoves.cpp
+++ ChessTests/TestBishopPossibleMoves.cpp
@@ -87,3 +87,31 @@ TEST(TestBishopPossibleMoves, Test_Moves_On_Ocupied_Squares_With_Enemy_Color)
 
 	ComparePositionLists(bishopPossibleMoves, bishopExpectedMoves);
 }
+
+TEST(TestBishopPossibleMoves, Test_Moves_While_King_In_Check_From_Rook)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			' ', 'K', ' ', 'R', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', 'b', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			' ', ' ', ' ', 'k', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	PositionList bishopPossibleMoves = game.GetPossibleMoves(Position(3, 6));
+	PositionList bishopExpectedMoves = { Position(0,3), Position(6,3) };
+
+	ComparePositionLists(bishopPossibleMoves, bishopExpectedMoves);
+}
