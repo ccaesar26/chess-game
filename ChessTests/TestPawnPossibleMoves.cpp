da9720ff7979799f4046fe2da7a96d9a91eb7458
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-21 12:39:14 +0300

    Update TestPawnPossibleMoves.cpp
    Create the same 3 tests but for black

--- ChessTests/TestPawnPossibleMoves.cpp
+++ ChessTests/TestPawnPossibleMoves.cpp
@@ -30,7 +30,7 @@ TEST(TestPawnPossibleMoves, Test_White_Moves_On_Empty_Squares)
 	EXPECT_EQ(ComparePositionLists(pawnPossibleMoves, pawnExpectedMoves), true);
 }
 
-TEST(TestPawnPossibleMoves, Test_Moves_On_Ocupied_Squares_With_Same_Color)
+TEST(TestPawnPossibleMoves, Test_White_Moves_On_Ocupied_Squares_With_Same_Color)
 {
 	// Declare board //
 
@@ -58,7 +58,7 @@ TEST(TestPawnPossibleMoves, Test_Moves_On_Ocupied_Squares_With_Same_Color)
 	EXPECT_EQ(ComparePositionLists(pawnPossibleMoves, pawnExpectedMoves), true);
 }
 
-TEST(TestPawnPossibleMoves, Test_Moves_On_Ocupied_Squares_With_Enemy_Color)
+TEST(TestPawnPossibleMoves, Test_White_Moves_On_Ocupied_Squares_With_Enemy_Color)
 {
 	// Declare board //
 
@@ -82,6 +82,92 @@ TEST(TestPawnPossibleMoves, Test_Moves_On_Ocupied_Squares_With_Enemy_Color)
 
 	PositionList pawnPossibleMoves = game.GetPossibleMoves(Position(5, 6));
 	PositionList pawnExpectedMoves = { Position(4,6),Position(4,5),Position(4,7) };
+
+	EXPECT_EQ(ComparePositionLists(pawnPossibleMoves, pawnExpectedMoves), true);
+}
+
+// black // 
+
+TEST(TestPawnPossibleMoves, Test_Black_Moves_On_Empty_Squares)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', 'P', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			'k', ' ', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::Black);
+
+	PositionList pawnPossibleMoves = game.GetPossibleMoves(Position(1, 6));
+	PositionList pawnExpectedMoves = { Position(2,6),Position(3,6) };
+
+	EXPECT_EQ(ComparePositionLists(pawnPossibleMoves, pawnExpectedMoves), true);
+}
+
+TEST(TestPawnPossibleMoves, Test_Black_Moves_On_Ocupied_Squares_With_Same_Color)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', 'P', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', 'P', ' ', 'P',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			'k', ' ', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::Black);
+
+	PositionList pawnPossibleMoves = game.GetPossibleMoves(Position(2, 6));
+	PositionList pawnExpectedMoves = { Position(3,6) };
+
+	EXPECT_EQ(ComparePositionLists(pawnPossibleMoves, pawnExpectedMoves), true);
+}
+
+TEST(TestPawnPossibleMoves, Test_Black_Moves_On_Ocupied_Squares_With_Enemy_Color)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', 'P', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', 'p', ' ', 'p',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			'k', ' ', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::Black);
+
+	PositionList pawnPossibleMoves = game.GetPossibleMoves(Position(2, 6));
+	PositionList pawnExpectedMoves = { Position(3,6),Position(3,5),Position(3,7) };
 
 	EXPECT_EQ(ComparePositionLists(pawnPossibleMoves, pawnExpectedMoves), true);
 }
