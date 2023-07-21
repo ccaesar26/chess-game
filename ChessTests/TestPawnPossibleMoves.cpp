a2a478cd6466cac4e7440217318a4a11c6a61764
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-21 12:47:44 +0300

    Update TestPawnPossibleMoves.cpp
    Test name : Test_White_Moves_While_King_Is_Pinned_By_Rook

--- ChessTests/TestPawnPossibleMoves.cpp
+++ ChessTests/TestPawnPossibleMoves.cpp
@@ -86,7 +86,6 @@ TEST(TestPawnPossibleMoves, Test_White_Moves_On_Ocupied_Squares_With_Enemy_Color
 	EXPECT_EQ(ComparePositionLists(pawnPossibleMoves, pawnExpectedMoves), true);
 }
 
-// black // 
 
 TEST(TestPawnPossibleMoves, Test_Black_Moves_On_Empty_Squares)
 {
@@ -170,4 +169,31 @@ TEST(TestPawnPossibleMoves, Test_Black_Moves_On_Ocupied_Squares_With_Enemy_Color
 	PositionList pawnExpectedMoves = { Position(3,6),Position(3,5),Position(3,7) };
 
 	EXPECT_EQ(ComparePositionLists(pawnPossibleMoves, pawnExpectedMoves), true);
+}
+
+TEST(TestPawnPossibleMoves, Test_White_Moves_While_King_Is_Pinned_By_Rook)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			'k', ' ', ' ', 'p', ' ', ' ', ' ', 'R',   // 6
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::Black);
+
+	PositionList pawnPossibleMoves = game.GetPossibleMoves(Position(6, 3));
+
+	EXPECT_EQ(pawnPossibleMoves.size(),0);
 }
