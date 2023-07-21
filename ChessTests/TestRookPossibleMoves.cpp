de5d0e518537c7bbdac93d42341870a08489818c
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-21 10:02:12 +0300

    Update TestRookPossibleMoves.cpp
    Test name : Test_Moves_On_Ocupied_Squares_With_Enemy_Color

--- ChessTests/TestRookPossibleMoves.cpp
+++ ChessTests/TestRookPossibleMoves.cpp
@@ -58,3 +58,32 @@ TEST(TestRookPossibleMoves, Test_Moves_On_Ocupied_Squares_With_Same_Color)
 
 	EXPECT_EQ(ComparePositionLists(rookPossibleMoves, rookExpectedMoves), true);
 }
+
+TEST(TestRookPossibleMoves, Test_Moves_On_Ocupied_Squares_With_Enemy_Color)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', 'H', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', 'H', 'r', ' ', ' ', 'H', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', 'H', ' ', ' ', ' ', ' ',   // 6
+			'k', ' ', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	PositionList rookPossibleMoves = game.GetPossibleMoves(Position(4, 3));
+	PositionList rookExpectedMoves = { Position(3, 3),Position(2, 3),Position(1, 3),Position(0, 3),
+		Position(5, 3),Position(6, 3),Position(4, 2),Position(4, 4),Position(4, 5),Position(4, 6) };
+
+	EXPECT_EQ(ComparePositionLists(rookPossibleMoves, rookExpectedMoves), true);
+}
