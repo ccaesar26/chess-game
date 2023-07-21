e9ef5314e6b30292b918b93904fb5b80a466b97e
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-21 17:22:40 +0300

    Fix bug in ChessGame.cpp and create test for IsGameOver

--- ChessTests/TestIsGameOverFunction.cpp
+++ ChessTests/TestIsGameOverFunction.cpp
@@ -101,3 +101,28 @@ TEST(TestIsGameOver, King_In_Check_From_One_Piece_2)
 
 	EXPECT_EQ(game.IsGameOver(), true);
 }
+
+TEST(TestIsGameOver, King_In_Check_From_One_Piece_3)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			'b', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			'R', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			'R', ' ', ' ', ' ', ' ', ' ', ' ', 'k'    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	EXPECT_EQ(game.IsGameOver(), false);
+}
