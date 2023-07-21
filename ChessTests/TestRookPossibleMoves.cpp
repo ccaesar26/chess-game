540b6bfcd14bde9fc10dbb33c17a282529a04611
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-21 10:16:37 +0300

    Update TestRookPossibleMoves.cpp
    Test name : Test_Moves_While_King_In_Check_From_Rook_2

--- ChessTests/TestRookPossibleMoves.cpp
+++ ChessTests/TestRookPossibleMoves.cpp
@@ -88,7 +88,7 @@ TEST(TestRookPossibleMoves, Test_Moves_On_Ocupied_Squares_With_Enemy_Color)
 	EXPECT_EQ(ComparePositionLists(rookPossibleMoves, rookExpectedMoves), true);
 }
 
-TEST(TestRookPossibleMoves, Test_Moves_While_King_In_Check_From_Rook)
+TEST(TestRookPossibleMoves, Test_Moves_While_King_In_Check_From_Rook_1)
 {
 	// Declare board //
 
@@ -112,6 +112,34 @@ TEST(TestRookPossibleMoves, Test_Moves_While_King_In_Check_From_Rook)
 
 	PositionList rookPossibleMoves = game.GetPossibleMoves(Position(4, 3));
 	PositionList rookExpectedMoves = { Position(4,1) };
+
+	EXPECT_EQ(ComparePositionLists(rookPossibleMoves, rookExpectedMoves), true);
+}
+
+TEST(TestRookPossibleMoves, Test_Moves_While_King_In_Check_From_Rook_2)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', 'R', ' ', ' ', ' ', ' ', ' ', 'r',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			' ', 'k', ' ', ' ', ' ', ' ', ' ', ' '    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	PositionList rookPossibleMoves = game.GetPossibleMoves(Position(1, 7));
+	PositionList rookExpectedMoves = { Position(1,1) };
 
 	EXPECT_EQ(ComparePositionLists(rookPossibleMoves, rookExpectedMoves), true);
 }
