e9a32ee151ead3e7d66c11f1df42127d3748a42b
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-21 17:30:10 +0300

    Update TestIsGameOverFunction.cpp
    Test name : King_In_Check_From_One_Piece_5

--- ChessTests/TestIsGameOverFunction.cpp
+++ ChessTests/TestIsGameOverFunction.cpp
@@ -151,3 +151,28 @@ TEST(TestIsGameOver, King_In_Check_From_One_Piece_4)
 
 	EXPECT_EQ(game.IsGameOver(), false);
 }
+
+TEST(TestIsGameOver, King_In_Check_From_One_Piece_5)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', 'R',   // 0
+			'B', 'B', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', 'r', ' ',   // 6
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', 'k'    // 7
+	};
+
+	// Tests //
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	EXPECT_EQ(game.IsGameOver(), true);
+}
