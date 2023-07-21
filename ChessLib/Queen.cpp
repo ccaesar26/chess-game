43a18c8ed7d32d2e0991cd3370900fc10a216b6b
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-21 15:23:00 +0300

    Update in TestQueenPossibleMoves.cpp and Queen.cpp
    Change the order of the directions in Queen cpp and the expected position for every test for queen

--- ChessLib/Queen.cpp
+++ ChessLib/Queen.cpp
@@ -12,21 +12,20 @@ PositionList Queen::GetPattern(Position currentPos, std::function<PiecePtr(Posit
 
 	// upper left
 	AddFinalPositionsByDirections(currentPos, Position(-1, -1), GetPieceFromBoard, finalPositions);
-	// upper right
-	AddFinalPositionsByDirections(currentPos, Position(-1, 1), GetPieceFromBoard, finalPositions);
-	// lower left
-	AddFinalPositionsByDirections(currentPos, Position(1, -1), GetPieceFromBoard, finalPositions);
-	// lower right
-	AddFinalPositionsByDirections(currentPos, Position(1, 1), GetPieceFromBoard, finalPositions);
-
 	// up
 	AddFinalPositionsByDirections(currentPos, Position(-1, 0), GetPieceFromBoard, finalPositions);
-	// down
-	AddFinalPositionsByDirections(currentPos, Position(1, 0), GetPieceFromBoard, finalPositions);
-	// left
-	AddFinalPositionsByDirections(currentPos, Position(0, -1), GetPieceFromBoard, finalPositions);
+	// upper right
+	AddFinalPositionsByDirections(currentPos, Position(-1, 1), GetPieceFromBoard, finalPositions);
 	// right
 	AddFinalPositionsByDirections(currentPos, Position(0, 1), GetPieceFromBoard, finalPositions);
+	// lower right
+	AddFinalPositionsByDirections(currentPos, Position(1, 1), GetPieceFromBoard, finalPositions);	
+	// down
+	AddFinalPositionsByDirections(currentPos, Position(1, 0), GetPieceFromBoard, finalPositions);
+	// lower left
+	AddFinalPositionsByDirections(currentPos, Position(1, -1), GetPieceFromBoard, finalPositions);
+	// left
+	AddFinalPositionsByDirections(currentPos, Position(0, -1), GetPieceFromBoard, finalPositions);
 
 	return finalPositions;
 }
