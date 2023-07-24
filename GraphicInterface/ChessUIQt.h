3abfe1dd58d42dbd63dc843118e24ee58cea374c
Cezar Constăndoiu <cezarconstandoiu@gmail.com>
2023-07-24 10:56:07 +0300

    Board update

--- GraphicInterface/ChessUIQt.h
+++ GraphicInterface/ChessUIQt.h
@@ -25,9 +25,10 @@ public:
     //Modify if necessary with your history representation
     void UpdateHistory();
     //Modify if necessary with your board representation
-    void UpdateBoard(const std::array<std::array<std::pair<PieceType, PieceColor>, 8>, 8>& newBoard);
+    void UpdateBoard();
     //Modify if necessary with your possible moves representation
     void HighlightPossibleMoves(const std::vector<std::pair<int, int>>& possibleMoves);
+    void UnhighlightPossibleMoves(const std::vector<std::pair<int, int>>& possibleMoves);
     //Modify or delete
     void StartGame();
     void ShowPromoteOptions();
@@ -57,52 +58,52 @@ private:
 };
 
 //TODO REMOVE THIS AFTER IMPLEMENTATION
-class Helper {
-public:
-    static std::array<std::array<std::pair<PieceType, PieceColor>, 8>, 8> getDefaultBoard() {
-        std::array<std::array<std::pair<PieceType, PieceColor>, 8>, 8> board;
-        for (int rank = 0; rank < 8; ++rank) {
-            for (int file = 0; file < 8; ++file) {
-                if (rank == 0 || rank == 7) {
-                    PieceColor color = (rank == 0) ? PieceColor::black : PieceColor::white;
-                    PieceType type;
-                    switch (file) {
-                    case 0:
-                    case 7:
-                        type = PieceType::rook;
-                        break;
-                    case 1:
-                    case 6:
-                        type = PieceType::knight;
-                        break;
-                    case 2:
-                    case 5:
-                        type = PieceType::bishop;
-                        break;
-                    case 3:
-                        type = PieceType::queen;
-                        break;
-                    case 4:
-                        type = PieceType::king;
-                        break;
-                    default:
-                        type = PieceType::none;
-                        break;
-                    }
-                    board[rank][file] = std::make_pair(type, color);
-                }
-                else if (rank == 1 || rank == 6) {
-                    // Set up the pawns row with black and white pawns respectively
-                    PieceColor color = (rank == 1) ? PieceColor::black : PieceColor::white;
-                    board[rank][file] = std::make_pair(PieceType::pawn, color);
-                }
-                else {
-                    // Empty squares for the rest of the board
-                    board[rank][file] = std::make_pair(PieceType::none, PieceColor::none);
-                }
-            }
-        }
-
-        return board;
-    }
-};
+//class Helper {
+//public:
+//    static std::array<std::array<std::pair<PieceType, PieceColor>, 8>, 8> getDefaultBoard() {
+//        std::array<std::array<std::pair<PieceType, PieceColor>, 8>, 8> board;
+//        for (int rank = 0; rank < 8; ++rank) {
+//            for (int file = 0; file < 8; ++file) {
+//                if (rank == 0 || rank == 7) {
+//                    PieceColor color = (rank == 0) ? PieceColor::black : PieceColor::white;
+//                    PieceType type;
+//                    switch (file) {
+//                    case 0:
+//                    case 7:
+//                        type = PieceType::rook;
+//                        break;
+//                    case 1:
+//                    case 6:
+//                        type = PieceType::knight;
+//                        break;
+//                    case 2:
+//                    case 5:
+//                        type = PieceType::bishop;
+//                        break;
+//                    case 3:
+//                        type = PieceType::queen;
+//                        break;
+//                    case 4:
+//                        type = PieceType::king;
+//                        break;
+//                    default:
+//                        type = PieceType::none;
+//                        break;
+//                    }
+//                    board[rank][file] = std::make_pair(type, color);
+//                }
+//                else if (rank == 1 || rank == 6) {
+//                    // Set up the pawns row with black and white pawns respectively
+//                    PieceColor color = (rank == 1) ? PieceColor::black : PieceColor::white;
+//                    board[rank][file] = std::make_pair(PieceType::pawn, color);
+//                }
+//                else {
+//                    // Empty squares for the rest of the board
+//                    board[rank][file] = std::make_pair(PieceType::none, PieceColor::none);
+//                }
+//            }
+//        }
+//
+//        return board;
+//    }
+//};
