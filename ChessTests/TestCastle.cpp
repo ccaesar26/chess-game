785abdc4712653258c53164adca771d0dd9c32d2
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-24 14:31:48 +0300

    Update TestCastle.cpp
    Test name : Test_White_Can_Not_Castle_3

--- ChessTests/TestCastle.cpp
+++ ChessTests/TestCastle.cpp
@@ -150,3 +150,31 @@ TEST(TestCastle, Test_White_Can_Not_Castle_2)
 
 	EXPECT_EQ(ComparePositionLists(kingPossbileMoves, kingExpectedMoves), true);
 }
+
+TEST(TestCastle, Test_White_Can_Not_Castle_3)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', 'p', 'p', 'p', ' ', ' ',   // 6
+			'r', 'h', ' ', ' ', 'k', ' ', 'h', 'r'    // 7
+	};
+
+	ChessGame game(alternativeBoard, EColor::White);
+
+	// Tests //
+
+	PositionList kingPossbileMoves = game.GetPossibleMoves(Position(7, 4));
+	PositionList kingExpectedMoves = { Position(7,5),Position(7,3) };
+
+	EXPECT_EQ(ComparePositionLists(kingPossbileMoves, kingExpectedMoves), true);
+}
