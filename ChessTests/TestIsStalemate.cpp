8e7dd26291e08dbce3ded5b08c1a018ff7206694
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-07-25 08:55:11 +0300

    Update TestIsStalemate.cpp
    Create 2 tests for Stalemate

--- ChessTests/TestIsStalemate.cpp
+++ ChessTests/TestIsStalemate.cpp
@@ -29,3 +29,57 @@ TEST(TestIsStaleMate, Test_Stale_Mate_1)
 
 	EXPECT_EQ(game.IsDraw(),true);
 }
+
+TEST(TestIsStaleMate, Test_Stale_Mate_2)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			'Q', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', 'k'    // 7
+	};
+
+	ChessGame game(alternativeBoard, EColor::Black);
+
+	// Tests //
+
+	game.MakeMovement(1, 0, 6, 5);
+
+	EXPECT_EQ(game.IsDraw(), true);
+}
+
+TEST(TestIsStaleMate, Test_Stale_Mate_3)
+{
+	// Declare board //
+
+	std::array<std::array<char, 8>, 8> alternativeBoard =
+	{
+		//   0    1    2    3    4    5    6    7
+
+			'K', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 0
+			'Q', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 1		
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 2
+			'p', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 3
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 4
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 5
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',   // 6
+			' ', ' ', ' ', ' ', ' ', ' ', ' ', 'k'    // 7
+	};
+
+	ChessGame game(alternativeBoard, EColor::Black);
+
+	// Tests //
+
+	game.MakeMovement(1, 0, 6, 5);
+
+	EXPECT_EQ(game.IsDraw(), false);
+}
