8b624275458dccf3317910010af24094f4cfeee9
Gabi-Plosnita <plosnitavalentingabriel@gmail.com>
2023-08-03 11:36:40 +0300

    Update PGNFormat.cpp

--- ChessLib/PGNFormat.cpp
+++ ChessLib/PGNFormat.cpp
@@ -1,5 +1,7 @@
 #include "PGNFormat.h"
 
+#include <fstream>
+
 PGNFormat::PGNFormat()
 	:m_PGNString("")
 {}
@@ -12,4 +14,12 @@ std::string PGNFormat::GetPGNFormat() const
 void PGNFormat::AddMove(const std::string& move)
 {
 	m_PGNString += move;
+}
+
+bool PGNFormat::Save(const std::string& filename)
+{
+	std::ofstream fout(filename);
+	fout << m_PGNString;
+	fout.close();
+	return true;
 }
